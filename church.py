6ec151fb6be93f76f3cac078d8b8667903fe745b
Daniel Ifediba <100448598+daniel-datasci@users.noreply.github.com>
2025-04-23 15:30:19 +0100

    Update church.py

--- church.py
+++ church.py
@@ -151,7 +151,7 @@ elif menu == "Reports":
         conv  = st.number_input("New Converts", min_value=0, step=1)
         first = st.number_input("First Timers", min_value=0, step=1)
         date_ = st.date_input("Date of Meeting", value=today)
-        if if st.form_submit_button("ADD SERVICE REPORT"):
+        if st.form_submit_button("ADD SERVICE REPORT"):
             row = {
               "Meeting Day": md, "Total Attn.": total,
               "Male Attn.": male, "Female Attn.": female,
